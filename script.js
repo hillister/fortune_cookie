26b30e06a451ab1c05820b1e82fb46704e20d356
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2025-04-11 12:02:16 +0100

    Added sound on click with image change

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let img = document.getElementById('img1');
 let newBtn = document.getElementById('newBtn');
+let crunch  = new Audio('crunch.mp3')
 
 img.addEventListener('click',function(){
+    crunch.play();
     changeImage();
     showButton();
+    
 })
 
 function changeImage(){
