4a9cc2e132734bbd762c40552a194d36920683db
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2025-04-11 11:42:44 +0100

    image changes when clicked and button appears also

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log(2+1)
+let img = document.getElementById('img1');
+let newBtn = document.getElementById('newBtn');
+
+img.addEventListener('click',function(){
+    changeImage();
+    showButton();
+})
+
+function changeImage(){
+    img.src = 'broken.png';
+}
+
+function showButton(){
+    newBtn.style.display = 'inline';
+}
