337f1d26d6b5d77dadb4680c539ab917048352c4
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2025-04-11 12:32:29 +0100

    added fade out to image

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ img.addEventListener('click',function(){
     crunch.play();
     changeImage();
     showButton();
-    
+    fadeOutImage();
 })
 
 function changeImage(){
@@ -16,3 +16,39 @@ function changeImage(){
 function showButton(){
     newBtn.style.display = 'inline';
 }
+
+
+function fadeOutImage() {
+    setTimeout(() => {
+        img.classList.add("fade-out");
+      }, 2000);
+}
+/*
+text for empty array:
+  "You've exhausted the cookie. Are you proud of yourself?",
+  "No more wisdom for you. Go live a little.",
+  "Even the cookie needs a break sometimes.",*/
+
+/*
+list of fortune cookie ideas:
+"You are seriously seeking wisdom from a cookie?"
+"Even this cookie questions its own existence."
+"You think therefore... you overthink."
+"You will discover nothing. And it will be profound."
+"The cookie sees all. It just doesn’t care."
+"Your destiny has been outsourced."
+"If life gives you lemons, question why it didn’t give cookies."
+*/
+
+
+/*
+options under button:
+"This is not how enlightenment works."
+"The cookie is disappointed that you are disappointed."
+The cookie judges in silence...
+"The next fortune may or may not help. Probably not."
+"You keep pushing the button. Who’s really in control here?"
+"Each click shatters another piece of the cookie’s fragile ego.
+you are breaking the cookie...and its heart
+*/
+
